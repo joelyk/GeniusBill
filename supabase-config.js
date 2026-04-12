6814b42a24d05e981cc77d4db147a5448f20109d
joelyk <acronymejoel@gmail.com>
2026-04-12 13:11:20 +0200

    Fix Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
-window.GB_SUPABASE_URL = 'https://qijhxgxsiuxyqqqbsufk.supabase.co';
+window.GB_SUPABASE_URL = 'https://qjjhxgxsiuxyqqqbsufk.supabase.co';
 window.GB_SUPABASE_ANON_KEY = 'sb_publishable_aHaJoSYVq3kWTxYPqHtcNQ_hVgcx8uF';
 window.GB_SUPABASE_ADMIN_EMAIL = 'nkilitech@gmail.com';
